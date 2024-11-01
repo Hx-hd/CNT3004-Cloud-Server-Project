429a1fbc808d3789a4e093d80af4469b29a6eae7
Harrison <ahnafgoat@gmail.com>
2024-11-01 17:29:20 -0400

    Very basic start for the server allowing the server to start and listen for clients.

--- server.py
+++ server.py
@@ -1,5 +1,19 @@
 import socket
+import threading
 
-##Test Comment pushing directly from VSCode
 
-host = ''
+## (Not sure yet on what to use for ip and port) 
+IP = ''
+PORT = ''
+
+
+ADDR = (IP, PORT)
+def main() :
+    print("[STARTING] Server is starting...")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    server.listen()
+    print(f"[LISTENING] Server is listening on {IP}:{PORT}")
+
+if __name__ == "__main__" :
+    main()
