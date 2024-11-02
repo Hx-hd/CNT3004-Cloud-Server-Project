c8ef1c773d8171ee9a7056229afea57708150310
Robert Baumgarten <112660207+rbaumgarten1119@users.noreply.github.com>
2024-11-02 19:09:39 -0400

    Basic connection setup to server
    Imported socket lib, added variables for server IP/port numbers (and buffer size), and added a function for connecting to the server.

--- client.py
+++ client.py
@@ -1,1 +1,14 @@
+import socket
+
+# Server ip and port not established yet
+SERVER_IP = ''
+SERVER_PORT = ''
+BUFFER_SIZE = 1024  # can edit as needed
+
+
+def connectToServer():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((SERVER_IP, SERVER_PORT))
+    print('Connected to server')
+    return client_socket
 
