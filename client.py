5b617d3f0375b3cdf3dd9b0ae58299c193eb8c48
Robert Baumgarten <112660207+rbaumgarten1119@users.noreply.github.com>
2024-11-02 19:27:43 -0400

    Create main function for client.py
    Added main function which starts the connection to server and includes UI elements allowing the user to choose which client-side actions to take.

--- client.py
+++ client.py
@@ -41,3 +41,52 @@ def createSubfolder(client_socket, subfolder):
 def deleteSubfolder(client_socket, subfolder):
     print("")  # temporary error-from-no-code prevention
     # TODO: implement deleteSubfolder
+
+def main():
+    # establish connection
+    client = connectToServer()
+
+    # prompt client with given command options
+    while True:
+        userInput = input("Enter a command:\n\tupload (filename)- upload file\n\tdownload (filename)- download file"
+                          "\n\tdelete (filename)- delete file\n\tdir - view files/subdirectories in file storage path"
+                          "\n\tcreate subfolder (subfolder name) - create subfolder"
+                          "\n\tdelete subfolder (subfolder name) - delete subfolder\n\texit - exit program\n")
+
+        # Exit
+        if userInput == "exit" or userInput == "Exit":
+            break
+
+        # Upload
+        elif userInput.startswith("upload") or userInput.startswith("Upload"):
+            filename = userInput.split(" ")[1]
+            uploadFile(client, filename)
+
+        # Download
+        elif userInput.startswith("download") or userInput.startswith("Download"):
+            filename = userInput.split(" ")[1]
+
+        # Dir
+        elif userInput == "dir" or userInput == "Dir":
+            viewDir()
+
+        # Delete file/subfolder
+        elif userInput.startswith("delete") or userInput.startswith("Delete"):
+            if userInput.split(" ")[1] == "subfolder" or userInput.split(" ")[1] == "Subfolder":
+                subfolder = userInput.split(" ")[1]
+                deleteSubfolder(client, subfolder)
+            filename = userInput.split(" ")[1]
+            deleteFile(client, filename)
+
+        # Create subfolder
+        elif userInput.startswith("create") or userInput.startswith("Create"):
+            subfolder = userInput.split(" ")[1]
+            createSubfolder(client, subfolder)
+
+        # Invalid response handling
+        else:
+            print("Invalid command. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
